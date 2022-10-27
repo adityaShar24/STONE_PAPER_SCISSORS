7beae889fb5749c80165ab66f6c89f7595dbb515
adityaShar24 <adityarajansharma1234@gmail.com>
2022-10-27 17:01:12 +0530

    initial commit

--- sps.py
+++ sps.py
@@ -1,24 +1,26 @@
 import random
-sps = ["S","P","SC"]
+sps = ["S", "P", "SC"]
 
-def paly():
+
+def play():
     value = random.choice(sps)
     user = input("let's start the game!  ")
     if value == user:
-        print("DRAW!")  
-    elif user=="P" and value =="S":
+        print("DRAW!")
+    elif user == "P" and value == "S":
         print("USER WINS")
-    elif user=="P" and value =="SC":
-        print("USER LOST") 
-    elif user=="SC" and value =="P":
+    elif user == "P" and value == "SC":
+        print("USER LOST")
+    elif user == "SC" and value == "P":
         print("USER WINS")
-    elif user=="S" and value =="P":
-        print("USER LOST") 
-    elif user=="S" and value =="SC":
+    elif user == "S" and value == "P":
+        print("USER LOST")
+    elif user == "S" and value == "SC":
         print("USER WINS")
-    elif user=="SC" and value =="S":
+    elif user == "SC" and value == "S":
         print("USER LOST")
     print(value)
-    paly()
+    play()
 
-paly()
+
+play()
