4f0fc2fa01c4498ccf36c5068a8777987585194a
adityaShar24 <adityarajansharma1234@gmail.com>
2022-10-27 16:51:50 +0530

    initial commit

---
+++ sps.py
@@ -0,0 +1,24 @@
+import random
+sps = ["S","P","SC"]
+
+def paly():
+    value = random.choice(sps)
+    user = input("let's start the game!  ")
+    if value == user:
+        print("DRAW!")  
+    elif user=="P" and value =="S":
+        print("USER WINS")
+    elif user=="P" and value =="SC":
+        print("USER LOST") 
+    elif user=="SC" and value =="P":
+        print("USER WINS")
+    elif user=="S" and value =="P":
+        print("USER LOST") 
+    elif user=="S" and value =="SC":
+        print("USER WINS")
+    elif user=="SC" and value =="S":
+        print("USER LOST")
+    print(value)
+    paly()
+
+paly()
